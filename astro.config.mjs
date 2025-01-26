9190a6f16bd3449c8b7ec0a0da6edeceaa0f1759
Luciana <97133350+LucianaSoledad86@users.noreply.github.com>
2025-01-26 16:29:43 -0300

    Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,23 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  output: "static",
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
+
+
+/*"static" todo es "prerender" por defecto
+
+a menos de que le ponga el "prerender" en "false"
+
+y el "server" es todo generado del lado del servidor
+
+a menos de que le ponga el "prerender" en "true". */
